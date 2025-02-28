3d86179ac19187b54b03978c93f07b9ca54209d2
Hien Luong Tran Hy <hyhien@gmail.com>
2025-02-28 11:30:26 +0700

    Truyen nhan du lieu

---
+++ Buoi06/Lab04BT02/Lab04BT02/Form1.cs
@@ -0,0 +1,29 @@
+﻿namespace Lab04BT02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKhoiTao_Click(object sender, EventArgs e)
+        {
+            int dong = 1, cot = 1;
+            if (!int.TryParse(txtSoDong.Text, out dong) || dong < 1)
+            {
+                MessageBox.Show("Nhập lại dòng");
+                txtSoDong.Focus();
+                return;
+            }
+            if (!int.TryParse(txtSoCot.Text, out cot) || cot < 1)
+            {
+                MessageBox.Show("Nhập lại cột");
+                txtSoCot.Focus();
+                return;
+            }
+            var myForm = new frmXuat(dong, cot);
+            myForm.Show();
+        }
+    }
+}
---
+++ Buoi06/Lab04BT02/Lab04BT02/frmXuat.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04BT02
+{
+    public partial class frmXuat : Form
+    {
+        public int SoDong { get; set; }
+        public int SoCot { get; set; }
+
+        const int H = 150; //rộng/cao button
+        const int S = 20; //khoảng cách giữa các button cũng như lề
+
+        public frmXuat(int soDong = 1, int soCot = 1)
+        {
+            InitializeComponent();
+            SoDong = soDong;
+            SoCot = soCot;
+
+            //Khởi tạo các Button và gắn vào form
+            Button button;
+            for (int i = 0; i < SoDong; i++)
+            {
+                for (int j = 0; j < SoCot; j++)
+                {
+                    button = new Button
+                    {
+                        Width = H,
+                        Height = H,
+                        Left = S + (H + S) * j,
+                        Top = S + (H + S) * i,
+                        Text = $"{i}, {j}"
+                    };
+                    button.Click += XuLyButtonClick;
+                    this.Controls.Add(button);
+                }
+            }
+            this.Size = new Size
+            {
+                Width = 3*S + (S + H) * SoCot,
+                Height = 4*S + (S + H) * SoDong,
+            };
+        }
+
+
+        private void XuLyButtonClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            MessageBox.Show($"Bạn vừa click button {btn.Text}");
+        }
+    }
+}
